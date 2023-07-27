2bf099cbeb1ecbbfc4b274e44666c0dfacc6bad4
Lee Yoonbin <younbihn@naver.com>
2023-07-27 21:54:11 +0900

    final

--- src/main/java/zerobase/weather/config/SwaggerConfig.java
+++ src/main/java/zerobase/weather/config/SwaggerConfig.java
@@ -1,32 +1,24 @@
 package zerobase.weather.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build().apiInfo(apiInfo());
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("title")
-                .description("description")
-                .version("1.0")
-                .build();
+    private Info apiInfo() {
+        return new Info()
+                .title("날씨 일기 프로젝트 :)")
+                .description("날씨 일기를 CRUD할 수 있는 백엔드 API입니다")
+                .version("2.0");
     }
 }
--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -1,5 +1,8 @@
 package zerobase.weather.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import zerobase.weather.domain.Diary;
@@ -16,30 +19,35 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
+    @Operation(summary = "일기 텍스트와 날씨를 이용해서 DB에 일기 저장", description = "DB에 저장할 수 있습니다.")
     @PostMapping("/create/diary")
-    void createDiary(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) {
+    void createDiary(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) @Parameter(name = "date", description = "일기를 저장할 날짜 ex) 2020-02-02") LocalDate date, @RequestBody String text) {
         diaryService.createDiary(date, text);
     }
 
+    @Operation(summary = "특정 날짜를 이용하여 해당 날짜의 DB를 조회", description = "특정 날짜의 DB를 조회할 수 있습니다.")
     @GetMapping("/read/diary")
-    List<Diary> readDiary(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate date) {
+    List<Diary> readDiary(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) @Parameter(name = "date", description = "조회할 날짜 ex) 2020-02-02") LocalDate date) {
         diaryService.readDiary(date);
         return diaryService.readDiary(date);
     }
 
+    @Operation(summary = "특정 날짜를 이용하여 해당 날짜 사이 기간의 DB를 조회", description = "특정 기간의 DB를 조회할 수 있습니다.")
     @GetMapping("/read/diaries")
-    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate startDate,
-                            @RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate endDate) {
+    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) @Parameter(name = "startDate", description = "조회할 기간의 시작 날 ex) 2020-02-02") LocalDate startDate,
+                            @RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) @Parameter(name = "endDate", description = "조회할 기간의 마지막 날 ex) 2020-02-02") LocalDate endDate) {
         return diaryService.readDiaries(startDate, endDate);
     }
 
+    @Operation(summary = "특정 날짜의 일기 DB를 수정", description = "특정 날짜의 DB를 수정할 수 있습니다.")
     @PutMapping("/update/diary")
-    void updateDiary(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) {
+    void updateDiary(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) @Parameter(name = "date", description = "일기를 수정할 날짜 ex) 2020-02-02") LocalDate date, @RequestBody String text) {
         diaryService.updateDiary(date, text);
     }
 
+    @Operation(summary = "특정 날짜의 일기 DB를 삭제", description = "특정 날짜의 DB를 삭제할 수 있습니다.")
     @DeleteMapping("/delete/diary")
-    void deleteDiary(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate date) {
+    void deleteDiary(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) @Parameter(name = "date", description = "일기를 삭제할 날짜 ex) 2020-02-02") LocalDate date) {
         diaryService.deleteDiary(date);
     }
 }
